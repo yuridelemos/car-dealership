b1c2ae837e9fbd12cb45c7f6cfff31c63cebd005
Yuri Ribeiro <yuridelemos@outlook.com>
2024-02-03 15:48:31 -0300

    Terminado a visão de comissão.

--- Controllers/SellerController.cs
+++ Controllers/SellerController.cs
@@ -8,4 +8,20 @@ internal class SellerController : Controller<Seller>
     public void Register() => base.Register(Sellers);
     public void List() => base.List(Sellers);
     public Seller SelectItem() => base.SelectItem(Sellers);
+
+    public void SalesCommission()
+    {
+        Console.WriteLine($"======= Seleção de vendedor =======");
+        var seller = SelectItem();
+        if (seller != null)
+        {
+            var totalComission = 0m;
+            totalComission = seller.Cars.Sum(p => p.Price * 0.05m);
+            totalComission += seller.Pieces.Sum(p => p.Price * 0.03m);
+            Console.WriteLine($"A comissão total de {seller.Name} é R$ {totalComission.ToString("N2")}");
+        }
+        else
+            Console.WriteLine($"Vendedor não encontrado(a).");
+        Console.ReadKey();
+    }
 }
